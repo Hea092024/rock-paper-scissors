78a6b5aadbfacdcbca86284f1e7be43592f50d47
Hector Acevedo <HEA092024@gmail.com>
2025-03-16 16:22:11 +0100

    Added input validation for user choice

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ getComputerChoice();
 
 function getHumanChoice() {
     let input = prompt ("Pick your choice:\n1 - Rock\n2 - Paper\n3 - Scissors\nPlease enter 1, 2, or 3:");
+
+    if (input === '1' || input === '2' || input ==='3'){
+        return input;
+    } else {
+        alert("Invalid choice! Please enter 1, 2 or 3.")
+        return null;
+    }
     // console.log(input)
 }
 
