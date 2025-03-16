c375ff1cc7165dc3dd7be2e5bf5d0d8befa0f78b
Hector Acevedo <HEA092024@gmail.com>
2025-03-16 14:23:43 +0100

    Added console.log for debugging

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 
 function getComputerChoice () {
     let choices = ["Rock", "Scissors","Paper"]
+    // console.log(choices)
     return choices [Math.floor(Math.random() * choices.length)]
 };
+
+getComputerChoice();
+// console.log(getComputerChoice())
