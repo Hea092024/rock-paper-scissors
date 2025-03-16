dccb0719ab17a65a009a6a796250eafd6c61447b
Hector Acevedo <HEA092024@gmail.com>
2025-03-16 14:33:55 +0100

    Added getHumanChoice function + added console.log for debugging

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function getComputerChoice () {
 
 getComputerChoice();
 // console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let input = prompt ("Pick your choice:\n1 - Rock\n2 - Paper\n3 - Scissors\nPlease enter 1, 2, or 3:");
+    // console.log(input)
+}
+
+// getHumanChoice() //commented out to prevent prompt to pop up all the time
+// console.log(getHumanChoice())
