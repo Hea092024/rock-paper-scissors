69c163483a6beae61785b59fbd3862023994d943
Hector Acevedo <HEA092024@gmail.com>
2025-03-16 16:49:32 +0100

    Added playRound function with switch statement for game logic

--- script.js
+++ script.js
@@ -2,35 +2,47 @@
 
 let humanScore = 0;
 let computerScore = 0;
+let combinedChoice = humanScore + "-" + computerScore;
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-function getComputerChoice () {
-    let choices = ["Rock", "Scissors","Paper"]
-    // console.log(choices)
-    return choices [Math.floor(Math.random() * choices.length)]
-};
+function getComputerChoice() {
+  let choices = ["Rock", "Scissors", "Paper"];
+  // console.log(choices)
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
 getComputerChoice();
 // console.log(getComputerChoice())
 
 function getHumanChoice() {
-    let input = prompt ("Pick your choice:\n1 - Rock\n2 - Paper\n3 - Scissors\nPlease enter 1, 2, or 3:");
+  let input = prompt(
+    "Pick your choice:\n1 - Rock\n2 - Paper\n3 - Scissors\nPlease enter 1, 2, or 3:"
+  );
 
-    if (input === '1' || input === '2' || input ==='3'){
-        return input;
-    } else {
-        alert("Invalid choice! Please enter 1, 2 or 3.")
-        return null;
-    }
-    // console.log(input)
+  if (input === "1" || input === "2" || input === "3") {
+    return input;
+  } else {
+    alert("Invalid choice! Please enter 1, 2 or 3.");
+    return null;
+  }
+  // console.log(input)
 }
 
 // getHumanChoice() //commented out to prevent prompt to pop up all the time
 // console.log(getHumanChoice())
 
-
-function playRound (humanSelection, computerSelection) {
-    
+function playRound(humanSelection, computerSelection) {
+  switch (combinedChoice) {
+    case "rock-paper":
+      console.log("You Lose! Paper beats Rock");
+      computerScore++;
+    case "paper - scissors":
+      console.log("You Lose! Scissors beats Paper");
+      computerScore++;
+      case "scissors - rock":
+        console.log("You Lose! Rock beats Scissors");
+        computerScore++;
+  }
 }
